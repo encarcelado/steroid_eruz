8725f9904cf9647aa51657dd4340b6582205d364
encarcelado <1560ford>
2020-01-17 18:32:53 +0500

    122

--- ERUZ_to_MySQL.py
+++ ERUZ_to_MySQL.py
@@ -1,9 +1,9 @@
-print("hello2")
+#print("hello2")
 
 import bs4 as bs
 import urllib.request
 
-sauce = urllib.request.urlopen("https://zakupki.gov.ru/epz/eruz/card/general-information.html?revisionId=143760").read()
+sauce = urllib.request.urlopen("https://zakupki.gov.ru/epz/eruz/card/general-information.html?reestrNumber=20008985").read()
 content = sauce.decode("utf-8")
 
 #print(content)
@@ -22,19 +22,74 @@ print(soup.title.text) #or string
 #address = soup.find("td", class_="noticeTdFirst fontBoldTextTd")
 #print(address.next_sibling.next_sibling.text)
 
-address = soup.find("td", text="ИНН")
-print(address.next_sibling.next_sibling.text)
+eruzNum = soup.find("span", text="Номер реестровой записи в ЕРУЗ").next_sibling.next_sibling.text
+print(eruzNum)
 
-address = soup.find("td", text="Номер реестровой записи в ЕРУЗ")
-print(address.next_sibling.next_sibling.text)
 
-address = soup.find("td", text="Адрес электронной почты")
-print(address.next_sibling.next_sibling.text)
+statusReg = soup.find("span", text="Статус регистрации").next_sibling.next_sibling.text
+print(statusReg)
 
-address = soup.find(class_="odd")
-print(address)
-address2 = address.find_all("td")
-print(address2[0].text)
+tipUchastnika = soup.find("span", text="Тип участника закупки").next_sibling.next_sibling.text
+print(tipUchastnika)
+
+dataReg = soup.find("span", text="Дата регистрации в ЕИС").next_sibling.next_sibling.text
+print(dataReg)
+
+fullCompanyName = soup.find("span", text="Полное наименование").next_sibling.next_sibling.text.title()
+print(fullCompanyName)
+
+shortCompanyName = soup.find("span", text="Сокращенное наименование").next_sibling.next_sibling.text
+print(shortCompanyName)
+
+companyAddress = soup.find("span", text="Адрес в пределах места нахождения").next_sibling.next_sibling.text.title()
+print(companyAddress)
+
+companyOkveds = soup.find("span", text="Код(ы) ОКВЭД").next_sibling.next_sibling
+
+
+#singleOkved = companyOkveds2.find_all("div")
+
+#print(singleOkved[0].text)
+
+for singleOkved in companyOkveds.find_all("div"):
+    print(singleOkved.text)
+
+
+
+
+companyINN = soup.find("span", text="ИНН").next_sibling.next_sibling.text.strip()
+print(companyINN)
+
+companyKPP = soup.find("span", text="КПП").next_sibling.next_sibling.text.strip()
+print(companyKPP)
+
+companyRegDate = soup.find("span", text="Дата постановки на учет в налоговом органе").next_sibling.next_sibling.text.strip()
+print(companyRegDate)
+
+companyOGRN = soup.find("span", text="ОГРН").next_sibling.next_sibling.text.strip()
+print(companyOGRN)
+
+bossFullName = soup.find("td", class_="tableBlock__col").text.title()
+bossTitle = soup.find("td", class_="tableBlock__col").next_sibling.next_sibling.text.title()
+bossINN = soup.find("td", class_="tableBlock__col").next_sibling.next_sibling.next_sibling.next_sibling.text
+print(bossFullName, bossTitle, bossINN)
+
+companyEmail = soup.find("span", text="Адрес электронной почты").next_sibling.next_sibling.text.replace(" ", "")
+print(companyEmail)
+
+companyPhone = soup.find("span", text="Контактный телефон").next_sibling.next_sibling.text.replace(" ", "")
+print(companyPhone)
+
+
+
+
+
+
+
+#address = soup.find(class_="odd")
+#print(address)
+#address2 = address.find_all("td")
+#print(address2[0].text)
 
 
 #for match1 in match
